e79e5e9e311644796b1d58f66aa6fe6318c2563a
fung11111111 <ifung9604@gmail.com>
2020-08-08 00:08:30 +0800

    add better solution

--- Problems/MinimumSwapArr.cpp
+++ Problems/MinimumSwapArr.cpp
@@ -51,6 +51,18 @@ int FindMinSwap(vector<int> arr) {
     
     return count;
     
+    // better solution
+//    int i,c=0,n=arr.size();
+//    for(i=0;i<n;i++)
+//    {
+//        if(arr[i]==(i+1))
+//            continue;
+//
+//        swap(arr[i],arr[arr[i]-1]);
+//        c++;
+//        i--;
+//    }
+//    return c;
     
 }
 
